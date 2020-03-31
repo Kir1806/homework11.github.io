8723b85da8f8125d3e9a65e472566e18cf8f3e62
Kirill Shlenkin <Shlyonkin.K@yandex.ru>
2020-03-31 21:49:05 +0300

    configure hash

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js' // прописываем хеш в имени файла
         },
 module: {
     rules: [{ // тут описываются правила
@@ -26,10 +27,12 @@ module: {
         new HtmlWebpackPlugin({
             // Означает, что:
             inject: false, // стили НЕ нужно прописывать внутри тегов
-            hash: true, // для страницы нужно считать хеш
+            //hash: true, // для страницы нужно считать хеш
             template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
             filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-          })
+          }),
+          new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'})
         ]
     
 };
