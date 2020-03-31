b6e460e182b86832f410407be0d085fc99891925
Kirill Shlenkin <Shlyonkin.K@yandex.ru>
2020-03-31 21:30:15 +0300

    gonfigure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -8,10 +11,25 @@ module.exports = {
         },
 module: {
     rules: [{ // тут описываются правила
-        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
-        use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
-        exclude: /node_modules/ // исключает папку node_modules
+                test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+                use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+                exclude: /node_modules/ // исключает папку node_modules
+            },
+            {
+                test: /\.css$/, // применять это правило только к CSS-файлам
+                use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
             }
+        ]    
+    },
+    plugins: [ 
+        new MiniCssExtractPlugin({filename: 'index.css'}),
+        new HtmlWebpackPlugin({
+            // Означает, что:
+            inject: false, // стили НЕ нужно прописывать внутри тегов
+            hash: true, // для страницы нужно считать хеш
+            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+          })
         ]
-    }
+    
 };
