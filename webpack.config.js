2c3e2378e19eb8a4bd78ef19eee82f41430dd0cb
Kirill Shlenkin <Shlyonkin.K@yandex.ru>
2020-04-01 22:09:15 +0300

    delete index in pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,33 +5,60 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development'; // создаем переменную для development-сборки
+const isProd =!isDev;
 const {CleanWebpackPlugin} = require('clean-webpack-plugin'); //очистка dist
 const filename = ext => isDev ? `[name].${ext}` : `[name].[chunkhash].${ext}`;
+//Оптимизация лоадеров
+const cssLoaders = extra => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                hmr: isDev,
+                reloadAll: true
+            }
+        },
+        'css-loader?sourceMap'
+    ]
+
+    if (extra) {
+        loaders.push(extra);
+    }
+
+    return loaders;
+}
 
 
 module.exports = {
+    devtool: isDev ? 'source-map' : '',
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js' // прописываем хеш в имени файла
+        filename: filename('js')//'[name].[chunkhash].js' // прописываем хеш в имени файла
         },
 module: {
-    rules: [{ // тут описываются правила
-                test: /\.js$/, // регулярное выражение, которое ищет все js файлы
-                use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
-                exclude: /node_modules/ // исключает папку node_modules
-            },
-            {
-                test: /\.css$/i, // применять это правило только к CSS-файлам
-                use: [ (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                 'css-loader',
-                 'postcss-loader'
-                ] // к этим файлам нужно применить пакеты, которые мы уже установили
-            },
+    rules: [
+        { // тут описываются правила
+            test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+            use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+            exclude: /node_modules/ // исключает папку node_modules
+        },
+        {
+            test: /\.css$/i, // применять это правило только к CSS-файлам
+            use: cssLoaders()
+               // use: [ (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+               //  'css-loader',
+               //  'postcss-loader'
+               // ] // к этим файлам нужно применить пакеты, которые мы уже установили
+        },
 // пример настройки плагина image-webpack-loader
-            {
-                test: /\.(png|jpg|gif|ico|svg)$/,
-                use: [
+        {
+            test: /\.s[ac]ss$/,
+            use: cssLoaders('sass-loader')
+        },
+        {
+            test: /\.(png|jpg|gif|ico|svg)$/,
+            use: [
                     'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
                     {
                     loader: 'image-webpack-loader',
@@ -39,12 +66,18 @@ module: {
                     },
                     ]
             },
-// заггрузка шрифтов
+// загрузка шрифтов
             {
                 test: /\.(eot|ttf|woff|woff2)$/,
                 loader: 'file-loader?name=./vendor/[name].[ext]'
             }           
         ]    
+    },
+    // избавляемся от дублей в JS файлах
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     plugins: [ 
         new MiniCssExtractPlugin({filename: filename('css')}),
@@ -60,9 +93,13 @@ module: {
         new HtmlWebpackPlugin({
             // Означает, что:
             inject: false, // стили НЕ нужно прописывать внутри тегов
-            //hash: false, // для страницы  не нужно считать хеш
-            //template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
-            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+            hash: true, // для страницы  нужно считать хеш
+            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+            filename: 'index.html', // имя выходного файла, то есть того, что окажется в папке dist после сборки
+            // Оптимизация HTML
+            minify: {
+                collapseWhitespace: isProd
+            }
           }),
         new WebpackMd5Hash(),
         new CleanWebpackPlugin(),
